3d1d964a96c8cc01af80c1e6abdf64c52ce157c4
Ben Napier <ben@napier.xyz>
2024-02-18 17:55:15 +0000

    add col width override

--- nvim/lua/benapie/set.lua
+++ nvim/lua/benapie/set.lua
@@ -25,4 +25,11 @@ vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
-vim.opt.colorcolumn = "80"
+local amzn = require("amzn")
+local amzn_col_width_override = amzn.get_col_width_override()
+
+if amzn_col_width_override ~= nil then
+	vim.opt.colorcolumn = tostring(amzn_col_width_override)
+else
+	vim.opt.colorcolumn = "80"
+end
