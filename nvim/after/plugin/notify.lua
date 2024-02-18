6b6b65699d1e48234aace04e6ab8e9e2d91beb8d
Ben Napier <ben@napier.xyz>
2024-02-18 17:47:20 +0000

    customize notify

---
+++ nvim/after/plugin/notify.lua
@@ -0,0 +1,11 @@
+local notify = require("notify")
+
+notify.setup({
+	max_width = 60,
+	render = "wrapped-compact",
+	stages = "static",
+})
+
+vim.keymap.set("n", "<leader>cn", function()
+	notify.dismiss()
+end)
