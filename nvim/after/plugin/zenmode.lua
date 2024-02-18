c7246910fa97a6bf419e57860705d7df049312e9
Ben Napier <ben@napier.xyz>
2024-02-18 16:45:13 +0000

    add keymap for widening zen

--- nvim/after/plugin/zenmode.lua
+++ nvim/after/plugin/zenmode.lua
@@ -1,13 +1,37 @@
 local zen = require("zen-mode")
 
-zen.setup({
-	window = {
-		width = 86,
-		height = 0.9,
-		options = {},
-	},
-})
+local zen_open = false
+
+function create_zen_config(width)
+	return {
+		window = {
+			width = width,
+			height = 0.9,
+		},
+		on_open = function()
+			zen_open = true
+		end,
+		on_close = function()
+			zen_open = false
+		end,
+	}
+end
+
+zen.setup(create_zen_config(86))
 
 vim.keymap.set("n", "<leader>zz", function()
 	zen.toggle()
+end)
+
+vim.keymap.set("n", "<leader>lrc", function()
+	zen.setup({
+		window = {
+			width = 120,
+			height = 0.9,
+		},
+	})
+	if zen_open == true then
+		zen.toggle()
+		zen.toggle()
+	end
 end)
