0ed4d65a7a9d33303eb53610e53eca49dc372192
Ben Napier <ben@napier.xyz>
2024-02-19 10:26:36 +0000

    remove notification from buf.hover

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -36,6 +36,26 @@ lsp_zero.on_attach(function(_, bufnr)
     end, opts)
 end)
 
+-- remove notifications from buf.hover
+vim.lsp.handlers["textDocument/hover"] = function(_, result, ctx, config)
+    config = config or {}
+    config.focus_id = ctx.method
+    if not (result and result.contents) then
+        return
+    end
+    local markdown_lines =
+        vim.lsp.util.convert_input_to_markdown_lines(result.contents)
+    markdown_lines = vim.lsp.util.trim_empty_lines(markdown_lines)
+    if vim.tbl_isempty(markdown_lines) then
+        return
+    end
+    return vim.lsp.util.open_floating_preview(
+        markdown_lines,
+        "markdown",
+        config
+    )
+end
+
 local amzn = require("amzn")
 
 require("mason").setup({})
