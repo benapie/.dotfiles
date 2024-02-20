4d7ba852c76d2b78ff7fb780fac01a72f04f9e3d
Ben Napier <benapie@amazon.com>
2024-02-20 02:28:51 +0000

    add cpath

--- nvim/lua/benapie/remap.lua
+++ nvim/lua/benapie/remap.lua
@@ -8,3 +8,9 @@ vim.keymap.set("n", "Q", "<nop>")
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+
+vim.api.nvim_create_user_command("Cpath", function()
+    local path = vim.fn.expand("%:p")
+    vim.fn.setreg("+", path)
+    vim.notify('Copied "' .. path .. '" to the clipboard!')
+end, {})
