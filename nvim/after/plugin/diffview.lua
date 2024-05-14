4025eaf9d2c98f75a33c98f2b585659ec7be2c21
Ben Napier <benapie@amazon.com>
2024-05-14 02:10:48 +0000

    add keymap for diffview

--- nvim/after/plugin/diffview.lua
+++ nvim/after/plugin/diffview.lua
@@ -1,7 +1,17 @@
-require("diffview").setup({
+local diffview = require("diffview")
+
+diffview.setup({
     file_panel = {
         win_config = {
             width = 60,
         },
     },
 })
+
+vim.keymap.set("n", "<leader>dv", function()
+    if next(require("diffview.lib").views) == nil then
+        vim.cmd("DiffviewOpen")
+    else
+        vim.cmd("DiffviewClose")
+    end
+end)
